f866e0d0e14a73fef3f2883f6c216f5221b77616
Andrest07 <57931603+Andrest07@users.noreply.github.com>
2022-09-18 17:13:46 +1000

    Unity Input Manager Setup and Player Input System

---
+++ The Beastmasters Grimoire/Assets/Scripts/Player/InputSystem.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSystem : MonoBehaviour
+{
+    public void W()
+    {
+
+    }
+
+    public void A()
+    {
+
+    }
+
+    public void S()
+    {
+
+    }
+
+    public void D()
+    {
+
+    }
+
+    public void Esc()
+    {
+
+    }
+
+    public void Tab()
+    {
+
+    }
+
+    public void LeftMouse()
+    {
+
+    }
+
+    public void RightMouse()
+    {
+
+    }
+
+    public void Q()
+    {
+
+    }
+
+    public void E()
+    {
+
+    }
+
+    public void Numpad1()
+    {
+
+    }
+
+    public void Numpad2()
+    {
+
+    }
+
+    public void Numpad3()
+    {
+
+    }
+
+    public void Numpad4()
+    {
+
+    }
+
+    public void Numpad5()
+    {
+
+    }
+
+    public void Numpad6()
+    {
+
+    }
+
+    public void Numpad7()
+    {
+
+    }
+
+    public void Numpad8()
+    {
+
+    }
+
+    public void Numpad9()
+    {
+
+    }
+
+    public void Numpad0()
+    {
+
+    }
+
+    public void F()
+    {
+
+    }
+
+    public void Shift()
+    {
+
+    }
+
+    public void C()
+    {
+
+    }
+}
