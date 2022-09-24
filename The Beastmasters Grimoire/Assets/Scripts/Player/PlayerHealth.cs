3755344d13a3e3c6080d8beb153400241f30ce84
Nick <56817601+naudley17@users.noreply.github.com>
2022-09-24 10:24:03 +1000

    Player Health
    Added TakeDamage and Death methods. Updated Regen methods.

--- The Beastmasters Grimoire/Assets/Scripts/Player/PlayerHealth.cs
+++ The Beastmasters Grimoire/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,7 +1,8 @@
 /*
-AUTHOR DD/MM/YY: Nick 20/09/22
+AUTHOR DD/MM/YY: Nick 22/09/22
 
 	- EDITOR DD/MM/YY CHANGES:
+    - Nick 22/09/22: Added TakeDamage and Death methods. Updated Regen methods.
 */
 using System.Collections;
 using System.Collections.Generic;
@@ -9,43 +10,71 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [Header("Health settings")]
+    [Header("Health Settings")]
     public float totalHealth; // Player's total health points
     public float currentHealth; // Player's current health points
     public float totalHealthMultiplier; // Player's health points if buffed
 
-    [Header("Health Regen settings")]
+    [Header("Health Regen Settings")]
     public float healthRegenRate; // Rate at which player regains health
     public float healthRegenDelay; // Time before health regen kicks back in
-    public float healthRegenDelayCurrent; // Current time delay
-    public float healthRegenMultiplier; // Player's health regen rate if buffed
+    public float healthRegenDelayCurrent; // Time delay remaining
+    public float healthRegenMultiplier; // Player's health regen if buffed
 
     [Header("Regen Booleans")]
     public bool isTakingDamage; // "Is player currently taking damage?"
+
+    // Dummy Variables
+    private float damageFromEnemy;
     
     void Start()
     {
-        currentHealth = totalHealth;
+        currentHealth = totalHealth; // Initial Health
+        healthRegenDelayCurrent = 0; // Initial Regen Delay
     }
 
     void Update()
     {
-        if (isTakingDamage == false && healthRegenDelayCurrent == 0)
+        // Regen Delay Timer
+        healthRegenDelayCurrent -= Time.deltaTime;
+
+        // Regen Trigger
+        if (isTakingDamage == false && healthRegenDelayCurrent <= 0)
         {
             RegenHealth();
         }
+
+        // Death Trigger
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void TakeDamage()
+    {
+        isTakingDamage = true; // Flagged for taking damage
+        currentHealth -= damageFromEnemy; // Take damage
+        healthRegenDelayCurrent = healthRegenDelay; // Set regen delay to max
     }
 
     void RegenHealth()
     {
-        if (healthRegenDelayCurrent == 0)
+        // If no delay and health less than total
+        if (healthRegenDelayCurrent <= 0 && currentHealth < totalHealth) 
         {
             currentHealth += (healthRegenRate * Time.deltaTime);
         }
         
+        // Set delay to max to reset and prevent getting overhealed
         else
         {
             healthRegenDelayCurrent = healthRegenDelay;
         }
+    }
+
+    void Death()
+    {
+        // Death Event
     }
 }
