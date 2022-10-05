8032f59869897e66d957c75ea4002b59fbbdb937
Nabins724 <nabins724@gmail.com>
2022-10-05 23:36:55 +1100

    Stamina Bar update.
    Stamina bar changes when we change value manually while playing game in unity but the value is not decreasing with the use of space to sprint.

---
+++ The Beastmasters Grimoire/Assets/Scripts/UI/StaminaBar.cs
@@ -0,0 +1,41 @@
+/*
+    AUTHOR DD/MM/YY: Nabin 03/10/2022
+
+    - EDITOR DD/MM/YY CHANGES:
+    -
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    //Referencing to the PlayerStamina script.    
+    public PlayerStamina playerStamina;
+    public Image fillImage;
+    private Slider slider;
+
+
+    /* Awake is called before the first frame update. */
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This code is to remove 1% fill on Stamina bar even when the health is 0.
+        if (slider.value <= slider.minValue){
+            fillImage.enabled = false;
+        }
+        if (slider.value > slider.minValue && !fillImage.enabled){
+            fillImage.enabled = true;
+        }
+
+        float fillValue = playerStamina.currentStamina/ playerStamina.totalStamina * 5;
+        slider.value = fillValue;
+
+    }
+}
