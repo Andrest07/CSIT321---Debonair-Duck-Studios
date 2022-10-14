0327c9ee492d485f84ea38156778317bd54ca0d6
LowerNorfair <88299726+LowerNorfair@users.noreply.github.com>
2022-10-14 16:34:09 +1100

    Main Menu
    Minor changes

--- The Beastmasters Grimoire/Assets/Scripts/UI/MenuButton.cs
+++ The Beastmasters Grimoire/Assets/Scripts/UI/MenuButton.cs
@@ -9,26 +9,16 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
 
-public class MainMenuButton : EventTrigger
+public class MenuButton : MonoBehaviour
 {
     private TextMeshProUGUI buttonText;
-    private Color32 hoverColour = new Color32(236, 221, 99, 255);
     private Color32 originalColour;
+    public Color32 hoverColour = new Color32(236, 221, 99, 255);
 
     private void Start()
     {
         buttonText = GetComponentInChildren<TextMeshProUGUI>();
         originalColour = buttonText.color;
-    }
-
-    public override void OnPointerEnter(PointerEventData eventData)
-    {
-        base.OnPointerEnter(eventData);
-    }
-
-    public override void OnPointerExit(PointerEventData eventData)
-    {
-        base.OnPointerExit(eventData);
     }
 
     public void EnterHover()
