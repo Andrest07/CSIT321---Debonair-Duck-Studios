23bb01bd3eae7c128ca1a2339cef178f9eaba8d2
Nabins724 <nabins724@gmail.com>
2022-12-15 09:44:44 +1100

    Settings Script

--- The Beastmasters Grimoire/Assets/SettingsMenu.cs
+++ The Beastmasters Grimoire/Assets/SettingsMenu.cs
@@ -12,10 +12,23 @@ using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+    // To Control Volume
     public AudioMixer audioMixer;
 
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
+    }
+
+    // To set the quality (Low, Medium and High).
+    public void setQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    // To enter the fullscreen mode.
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
