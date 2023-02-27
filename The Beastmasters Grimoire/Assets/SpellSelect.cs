7d1c68afdfc8b3fbdd2b78eeab0e1544c1235faa
Nick <56817601+naudley17@users.noreply.github.com>
2023-02-27 14:15:07 +1100

    HUD Update
    HUD changes and BetterUI packages.

---
+++ The Beastmasters Grimoire/Assets/SpellSelect.cs
@@ -0,0 +1,25 @@
+/*
+    AUTHOR DD/MM/YY: Nick 19/01/2023
+
+    - EDITOR DD/MM/YY CHANGES:
+    -
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellSelect : MonoBehaviour
+{
+    // Awake is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
