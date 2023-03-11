bd74a71a866b487fb06c59435fe4a3075daec6ff
Ksdb216 <ksdb216@uowmail.edu.au>
2023-03-11 13:11:34 +1100

    Revert "More small Changes"
    This reverts commit 159d1fbc56cf4999e3e1a7233b6e5737d8bca487.

--- The Beastmasters Grimoire/Assets/Scripts/Enemy/EnemyCapture.cs
+++ The Beastmasters Grimoire/Assets/Scripts/Enemy/EnemyCapture.cs
@@ -17,7 +17,7 @@ public class EnemyCapture : MonoBehaviour
     private EnemyHealth enemyHealth;
     public float captureAmount = 0;
     public float captureMultiplyer;
-    private float hasCaptured;
+    private int hasCaptured;
     private float healthMultiplier;
 
 
@@ -35,11 +35,11 @@ public class EnemyCapture : MonoBehaviour
 
         if (hasCaptured == 0)
         {
-            hasCaptured = GameManager.instance.GetBeastiary(enemyScriptableObject) ? 1f : 0.25f;
+            hasCaptured = GameManager.instance.GetBeastiary(enemyScriptableObject) ? 1 : 0;
         }
-        healthMultiplier = enemyHealth.totalHealth/enemyHealth.currentHealth;
+        healthMultiplier = Mathf.Sqrt(enemyHealth.totalHealth/enemyHealth.currentHealth);
 
-        captureMultiplyer = (hasCaptured)*healthMultiplier; //Will be expanded with better calculations later
+        captureMultiplyer = (1 + hasCaptured)*healthMultiplier; //Will be expanded with better calculations later
         captureAmount += captureMultiplyer * power;
 
         enemyController.UpdateCapturehBar(captureAmount);
