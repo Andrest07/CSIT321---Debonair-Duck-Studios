5ea4cf9f54967c3cd278979fd8cc566e4f31685b
Ksdb216 <ksdb216@uowmail.edu.au>
2023-03-12 13:22:25 +1100

    Tutorial WIP

---
+++ The Beastmasters Grimoire/Assets/Scripts/Game Systems/TutorialScript.cs
@@ -0,0 +1,30 @@
+/*
+AUTHOR DD/MM/YY:
+
+	- EDITOR DD/MM/YY CHANGES:
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.position -= (other.transform.position - transform.position)*0.25f;
+        }
+    }
+}
