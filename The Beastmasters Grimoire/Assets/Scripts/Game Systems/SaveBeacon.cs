8ca365351fc653f785d48723a0a65eb389675112
Ksdb216 <ksdb216@uowmail.edu.au>
2023-05-21 21:17:07 +1000

    Revert "death fix"
    This reverts commit fcd1b634e599f1435758dbeccb2fcd40dbde86aa.

--- The Beastmasters Grimoire/Assets/Scripts/Game Systems/SaveBeacon.cs
+++ The Beastmasters Grimoire/Assets/Scripts/Game Systems/SaveBeacon.cs
@@ -17,7 +17,6 @@ using UnityEngine.UI;
 public class SaveBeacon : MonoBehaviour
 {
     private PlayerHealth playerH;
-    public GameObject DeathScreen;
     private DeathMenuScript DeathScript;
     //[Header("Scriptable Object")]
     public SaveBeaconScriptableObject beaconData;
@@ -27,8 +26,8 @@ public class SaveBeacon : MonoBehaviour
     void Start()
     {
         //playerHealth = player.GetComponent<PlayerHealth>();
-        //DeathScreen = GameObject.FindGameObjectWithTag("DeathScreen");
-        DeathScript =  DeathScreen.GetComponent<DeathMenuScript>();
+        //DeathScreen = GameObject.Find("DeathScreen");
+        DeathScript =  GameManager.instance.GetComponent<DeathMenuScript>();
         //FastTravelMenu = GameObject.Find("FastTravelMenu");
         playerH = PlayerManager.instance.GetComponent<PlayerHealth>();
         FastTravel = GameManager.instance.GetComponent<FastTravelScript>();
@@ -38,7 +37,7 @@ public class SaveBeacon : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
-        {   Debug.Log("CheckPoint set");
+        {
             DeathScript.checkpointLocation = transform.position;
             playerH.attunedBeacon = beaconData;
             GameManager.instance.GetComponent<FastTravelScript>().UnlockBeacon(beaconData); 
